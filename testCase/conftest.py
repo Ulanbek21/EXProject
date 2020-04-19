5bdd188f311621942c9e0e292f0415193cf5bdbf
Ulanbek21 <Ulanbek.Shaidullaev@gmail.com>
2020-04-19 03:08:02 -0400

    comment

--- testCase/conftest.py
+++ testCase/conftest.py
@@ -5,21 +5,17 @@ from selenium import webdriver
 
 
 @pytest.fixture()
-def setup(browser):
-    print('Launchig chrom browser')
-    if browser =='chrome':
-        driver = webdriver.Chrome()
-        print('Launching chrome browser')
-    elif browser == 'firefox':
-        driver = webdriver.Firefox()
-    return  driver
+def setUp():
+    driver = webdriver.Chrome()
+    yield
+    driver.quit()
     
 # This method will get value from CLI(comman line interfeis) /////  HOOKS
-# pytest -v -s --html=report.html pytestTests\CommandLine\test_CommandLine.py  --browser  chrome
-def pytest_addoption(parser):
-    parser.addoption('--browser')
+# pytest -v -s --html=report.html pytestTests\CommandLine\test_CommandLine.py  --driver  chrome
+# def pytest_addoption(parser):
+#     parser.addoption('--driver')
 
-# This method we need to write on emore fixture method
-@pytest.fixture()
-def browser(request):# This will return the browser value to set up method
-    return request.config.getoption('--browser')
+# # This method we need to write on emore fixture method
+# @pytest.fixture()
+# def driver(request):# This will return the driver value to set up method
+#     return request.config.getoption('--driver')
--- testCase/test_HomePage.py
+++ testCase/test_HomePage.py
@@ -1,11 +1,49 @@
 from selenium import webdriver
 import time
 import pytest
+# from EXProject.pageObject.homePage import OpenBrowser
 
+
+
+url = 'https://www.expedia.com/'
+driver = webdriver.Chrome()
 
 def test_Home_Page():
-    driver = webdriver.Chrome()
-    driver.implicitly_wait(5)
+    driver.get(url)
+    driver.implicitly_wait(20)
     driver.maximize_window()
-    driver.get('https://www.expedia.com/')
     assert driver.title == 'Expedia Travel: Search Hotels, Cheap Flights, Car Rentals & Vacations'
+
+# 1) Click on hotels 
+# 2) search for city
+# 3) validate that 20 hotels are displayed in results
+
+def test_click_hotel():
+    hotel = driver.find_element_by_id('primary-header-hotel').click()
+
+def test_find_city():
+    ser_city = driver.find_element_by_xpath("//input[@id='hotel-destination-hlp']").send_keys('Dallas')
+
+def test_Dallas_click():
+    driver.find_element_by_xpath('//*[@id="aria-option-0"]/div[2]/strong').click()
+
+def test_search_button():
+    driver.find_element_by_xpath('//*[@id="gcw-hotel-form-hlp"]/div[13]/label/button').click()
+    
+
+# def hotels():
+#     number_of_hotels = driver.find_elements_by_xpath("//div[@class='uitk-cell all-cell-3-4 all-x-gutter-six']")
+#     print('Rows:',len(number_of_hotels))
+
+# def cal():
+#     cal_but = driver.find_element_by_id('hotels-check-in-btn').click()
+
+time.sleep(5)
+
+handles = driver.switch_to.window
+def hotels():
+    number_of_hotels = len(driver.find_elements_by_xpath("//div[@class='uitk-cell all-cell-3-4 all-x-gutter-six']"))
+    # print('Rows:',len(number_of_hotels))
+
+    
+    
